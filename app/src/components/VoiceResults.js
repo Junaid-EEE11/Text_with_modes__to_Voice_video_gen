16dad42ed5021dad1054cdef4b010ac84db039e8
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-11 07:32:57 +0600

    Create VoiceResults.js

---
+++ app/src/components/VoiceResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VoiceResults.css';
+
+function VoiceResults({ modifiedVoice, sentiment }) {
+  return (
+    <div className="voice-results">
+      {/* ... existing JSX ... */}
+    </div>
+  );
+}
+
+export default VoiceResults;
