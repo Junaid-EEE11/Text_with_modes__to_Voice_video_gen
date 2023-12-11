fedeb008c5079b189d00b2a10c39c0e97a3b633d
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-11 07:27:18 +0600

    Update and rename app.js to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -6,14 +6,20 @@ npm start
 
 import React, { useState } from 'react';
 import axios from 'axios';
+import './App.css'; // Import a separate CSS file for styling
 
 function App() {
   const [text, setText] = useState('');
   const [mode, setMode] = useState('comedy');
   const [modifiedVoice, setModifiedVoice] = useState(null);
   const [sentiment, setSentiment] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async () => {
+    setLoading(true);
+    setError(null);
+
     try {
       const response = await axios.post('http://localhost:5000/modify_voice', {
         text: text,
@@ -24,6 +30,9 @@ function App() {
       setSentiment(response.data.sentiment);
     } catch (error) {
       console.error('Error modifying voice:', error.message);
+      setError('Error modifying voice. Please try again.');
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -31,12 +40,16 @@ function App() {
     <div className="App">
       <h1>Voice Modification App</h1>
 
-      <div>
+      <div className="input-section">
         <label>Enter Text:</label>
-        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Type your text here..."
+        />
       </div>
 
-      <div>
+      <div className="input-section">
         <label>Select Mode:</label>
         <select value={mode} onChange={(e) => setMode(e.target.value)}>
           <option value="comedy">Comedy</option>
@@ -46,12 +59,20 @@ function App() {
         </select>
       </div>
 
-      <div>
-        <button onClick={handleSubmit}>Submit</button>
+      <div className="button-section">
+        <button onClick={handleSubmit} disabled={loading}>
+          {loading ? 'Processing...' : 'Submit'}
+        </button>
       </div>
 
+      {error && (
+        <div className="error-message">
+          <p>{error}</p>
+        </div>
+      )}
+
       {modifiedVoice && (
-        <div>
+        <div className="result-section">
           <h2>Modified Voice</h2>
           <audio controls>
             <source src={modifiedVoice} type="audio/mpeg" />
@@ -61,7 +82,7 @@ function App() {
       )}
 
       {sentiment && (
-        <div>
+        <div className="result-section">
           <h2>Sentiment Analysis</h2>
           <p>Sentiment: {sentiment}</p>
         </div>
